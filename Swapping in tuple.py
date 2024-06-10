7c726c44249967d2142473fb2012c586d56870e8
Sauravsinghhh <152403879+Sauravsinghhh@users.noreply.github.com>
2024-06-10 16:27:05 +0530

    File By Saurav

---
+++ Swapping in tuple.py
@@ -0,0 +1,11 @@
+person = ("Saaurrav", 20, "Engineer")
+name, age, profession = person
+print("Name:", name)
+print("Age:", age)
+print("Profession:", profession)
+
+# Swapping values
+a = 5
+b = 10
+a, b = b, a
+print("After swapping: a =", a, "b =", b)
