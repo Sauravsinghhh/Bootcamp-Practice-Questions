62201aaed42f7e9e8bf85dbce150de12a64c0213
Sauravsinghhh <152403879+Sauravsinghhh@users.noreply.github.com>
2024-06-10 16:29:05 +0530

    File By Saurav

---
+++ Add or Remove in Sets.py
@@ -0,0 +1,13 @@
+Alphabets = {"A", "B", "C"}
+Alphabets.add("D")
+print("Set after adding 'D':", Alphabets)
+
+Alphabets.update(["E", "F"])
+print("Set after adding 'E' and 'F':", Alphabets)
+
+Alphabets.remove("B")
+print("Set after removing 'B':", Alphabets)
+
+
+Alphabets.discard("G")  
+print("Set after discarding 'pear':", Alphabets)
