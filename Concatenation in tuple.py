0c89d4cc155f3302639077d8a01c89d132e3bc30
Sauravsinghhh <152403879+Sauravsinghhh@users.noreply.github.com>
2024-06-10 16:26:41 +0530

    File By Saurav

---
+++ Concatenation in tuple.py
@@ -0,0 +1,10 @@
+tuple1 = (1, 2, 3)
+tuple2 = (4, 5, 6)
+concatenated_tuple = tuple1 + tuple2
+print("Concatenated tuple:", concatenated_tuple) #Concatentaion is combining multiple tuples into a single tuple
+
+repeated_tuple = tuple1 * 3
+print("Repeated tuple:", repeated_tuple)
+
+element_exists = 2 in tuple1
+print("Does 2 exist in tuple1?", element_exists)
