f2dd414d206c502f3dc38dc9faa39bf5659febf0
Sauravsinghhh <152403879+Sauravsinghhh@users.noreply.github.com>
2024-06-11 16:49:28 +0530

    File By Saurav

---
+++ tkinter basic(3).py
@@ -0,0 +1,5 @@
+from tkinter import *
+root = Tk()
+x = Label(root,text="Saurav is Good Human")
+x.pack()
+root.mainloop()
---
+++ tkinter basics(1).py
@@ -0,0 +1,14 @@
+import tkinter as tk
+from tkinter import messagebox
+
+def on_button_click():
+    messagebox.showinfo("Message", "Hello, Saurav!")
+
+root = tk.Tk()
+root.title("Tkinter Example")
+root.geometry("300x200")
+
+button = tk.Button(root, text="Click Me", command=on_button_click)
+button.pack(pady=20)
+
+root.mainloop()
